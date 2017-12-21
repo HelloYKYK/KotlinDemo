2f1e31c09c8b5c432058d81f4acebeaf6c003d0e
tsts <tstsolykyk@163.com>
2017-12-21 22:36:33 +0800

    ok

--- app/src/main/java/com/toy/myapplication/MainActivity.kt
+++ app/src/main/java/com/toy/myapplication/MainActivity.kt
@@ -6,12 +6,8 @@ import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-//import kotlinx.android.synthetic
-
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,67 +17,51 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-
     override fun onClick(v: View?) {
+        if (v?.id==R.id.tv_result)return
         if (v is TextView) {
             when (v.text) {
-                " = " -> computeAll(str)
-                " + " -> computeAdd()
-                " - " -> computeSubtraction()
-                " x " -> computeMultiplication()
-                "AC" -> clearAll()
-                else -> doElse(v.text)
+                " = " -> {
+                    if (tv_result.text.isEmpty()) return
+                    when (suanfa) {
+                        "+" -> result += tv_result.text.toString().toDouble()
+                        "-" -> result -= tv_result.text.toString().toDouble()
+                        "x" -> result *= tv_result.text.toString().toDouble()
+                    }
+                    var resultStr = result.toString()
+                    resultStr = if (resultStr.endsWith(".0")) resultStr.replace(".0", "") else resultStr
+                    tv_result.text = resultStr
+                    str=""
+                    result = 0.0
+                }
+                " + " -> {
+                    clearStatus("+")
+                    suanfa = "+"
+                }
+                " - " -> {
+                    clearStatus("-")
+                    suanfa = "-"
+                }
+                " x " -> {
+                    clearStatus("x")
+                    suanfa = "x"
+                }
+                "AC" -> {
+                   clearStatus("")
+                }
+                else -> {
+                    str += v.text
+                    tv_result.text = str
+                }
             }
         }
     }
 
-    private fun computeMultiplication() {
-        result = tv_result.text.toString().toInt()
-        tv_result.text = ""
+    private fun clearStatus(fuhao:String) {
+        result = if (tv_result.text.isEmpty()) 0.0 else tv_result.text.toString().toDouble()
+        tv_result.text = fuhao
         str = ""
-        suanfa = "x"
-
     }
-
-    private fun computeSubtraction() {
-
-        result = tv_result.text.toString().toInt()
-        tv_result.text = ""
-        str = ""
-        suanfa = "-"
-
-    }
-
-    private fun clearAll() {
-        tv_result.text = ""
-        str = ""
-        result = 0
-    }
-
-    private fun doElse(text: CharSequence?) {
-        str += text
-        tv_result.text = str
-    }
-
-
-    private fun computeAdd() {
-        result = tv_result.text.toString().toInt()
-        tv_result.text = ""
-        str = ""
-        suanfa = "+"
-    }
-
-    private fun computeAll(str: String) {
-        when (suanfa) {
-            "+" -> result += tv_result.text.toString().toInt()
-            "-" -> result -= tv_result.text.toString().toInt()
-            "x" -> result *= tv_result.text.toString().toInt()
-
-        }
-        tv_result.text = result.toString()
-        result = 0
-    }
-
 
 
 }
--- app/src/main/java/com/toy/myapplication/Model.kt
+++ app/src/main/java/com/toy/myapplication/Model.kt
@@ -4,6 +4,6 @@ package com.toy.myapplication
  * Created by toy on 2017/12/21.
  */
 var str = ""
-var result = 0
+var result = 0.0
 var suanfa = "+"
 
